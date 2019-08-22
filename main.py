231e056a6bf8f339ccc9f69d4ed20526cb654c7a
Alain Garcia <alaingarcia@ufl.edu>
2019-08-21 21:05:22 -0400

    Cleaned up main.py
    - Added trade function to account class
    - Added some comments to indicate what things did (where trading rules are)

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import stock_trading_sim as st
 import pandas as pd
 
-ticker_list = ['AAPL','V','MSFT','AMZN','FB','GOOG','JPM','UNH','BAC','MA']
-account = st.account(cash=1000000, risk=1, stock_list=ticker_list)
-suffix = '.USUSD_Candlestick_4_Hour_ASK_30.01.2018-31.07.2019.csv'
+if __name__ == '__main__':
+    ticker_list = ['AAPL','V','MSFT','AMZN','FB','GOOG','JPM','UNH','BAC','MA']
+    file_suffix = '.USUSD_Candlestick_4_Hour_ASK_30.01.2018-31.07.2019.csv'
+    data_folder = 'data/'
 
-for ticker in account.stock_list:
-    df = pd.read_csv('data/' + ticker + suffix, sep=',')
-    st.dataset_dict[ticker] = st.dataset(df)
-
-# only works if all the datasets are consistent with each other
-# same time periods, same length of time, etc.
-for i in range(len(st.dataset_dict[ticker_list[0]].df.index)):
-    st.trade(st.dataset_dict, account, i)
-
-account.print_results(st.dataset_dict)
+    account = st.account(cash=1000000, risk=1, stock_list=ticker_list)  
+    account.initializeDatasets(file_suffix, data_folder)
+    account.trade()
+    account.print_results(st.dataset_dict)
--- stock_trading_sim.py
+++ stock_trading_sim.py
@@ -7,6 +7,12 @@ from itertools import repeat
 import math
 import ta
 
+# dataset_dict holds all the datasets
+dataset_dict = {}
+
+# current_price_dict holds all the current prices
+current_price_dict = {}
+
 # dataset object used to hold technical analysis data for a certain stock
 class dataset():
   def __init__(self, df):
@@ -55,6 +61,11 @@ class account():
           self.portfolio[ticker] = []
       self.portfolio_value = 0
       self.account_value = cash
+  
+  def initializeDatasets(self, file_suffix, data_folder='data/'):
+    for ticker in self.stock_list:
+        df = pd.read_csv(data_folder + ticker + file_suffix, sep=',')
+        dataset_dict[ticker] = dataset(df)
 
   # recalculates the portfolio_value based on current prices
   def portfolioValue(self, current_price_dict):
@@ -94,17 +105,60 @@ class account():
       self.sell_orders += 1
     self.cash = round(self.cash, 2)
     print("{} - SELL {} {} @ ${} ea, cash: {}, account_value: {}".format(time, amount_to_sell, ticker, current_stock_price, self.cash, self.account_value))
-  
-  def print_stocks(self):
-    for ticker in self.stock_list:
-      if len(self.portfolio[ticker]) > 0:
-        for stock in self.portfolio[ticker]:
-          print('{}: AAPL, price_bought: {}'.format(stock.time_bought, stock.price_bought))
     
   def amount_to_buy(self, current_stock_price, account_value, equity_percent):
     cash_used_to_buy = equity_percent * account_value
     return math.floor(cash_used_to_buy/current_stock_price)
 
+  # trades (buy/sell) at current price using available cash
+  def trade(self, dataset_dict=dataset_dict):
+    
+    # only works if all the datasets are consistent with each other
+    # same time periods, same length of time, etc.
+
+    # gets the amount of rows in dataset, run potential trades
+    for i in range(len(dataset_dict[self.stock_list[0]].df.index)):
+
+      # update current price dict for accurate account value
+      for ticker in dataset_dict:
+        current_price_dict[ticker] = dataset_dict[ticker].close.iloc[i]
+      
+      self.account_value = self.accountValue(current_price_dict)
+
+      for ticker in dataset_dict:
+        # shorthand
+        df = dataset_dict[ticker].df
+        close = dataset_dict[ticker].close
+
+        # calculate necessary technical analysis indicators for each stock/ticker
+        if i < len(df['rsi']) - 1:
+          rsi = round(df['rsi'].iloc[i], 2)
+          rsi_next = round(df['rsi'].iloc[i+1], 2)
+          rsi_prev = round(df['rsi'].iloc[i-1], 2)
+          sma1 = round(df['sma14'].iloc[i], 2)
+          sma2 = round(df['sma30'].iloc[i], 2)
+          sma3 = round(df['sma50'].iloc[i], 2)
+          sma4 = round(df['sma200'].iloc[i], 2)
+          lowerkelt = round(df['lowerkelt'].iloc[i], 2)
+          upperkelt = round(df['upperkelt'].iloc[i], 2)
+          lowerboll = round(df['lowerboll'].iloc[i], 2)
+          upperboll = round(df['upperboll'].iloc[i], 2)
+
+          current_stock_price = close.iloc[i]
+          prev_stock_price = close.iloc[i-1]
+          time = df['Local time'].iloc[i]
+
+          # ------ TRADING RULES ------
+
+          # Buys
+          if ((rsi < 50) & (sma1>sma2)) or ((rsi > 50) & (sma3>sma4)):
+            self.buy(ticker, current_stock_price, amount_to_buy=1, time=time)
+
+          # Sells
+          elif (((rsi > 75) & (sma1<sma2)) or ((rsi < 75) & (sma3<sma4))) & (len(self.portfolio[ticker]) > 0):
+            self.sell(ticker, current_stock_price, amount_to_sell=1, time=time)
+
+  # Calculates ROI based on technical analysis trading and compares it to buy and hold ROI
   def print_results(self, dataset_dict):
     buy_hold_start, buy_hold_end = 0, 0
 
@@ -128,49 +182,3 @@ class account():
     print('--------Buy and Hold--------')
     ROI = ((buy_hold_end - buy_hold_start) / buy_hold_start) * 100
     print('Return on Investment: {}%'.format(round(ROI, 4)))
-
-# dataset_dict holds all the datasets
-dataset_dict = {}
-
-# current_price_dict holds all the current prices
-current_price_dict = {}
-
-def trade(dataset_dict, account, index):
-  # index is used to iterate through each entry in the dataset
-  i = index
-
-  # update current price dict for accurate account value
-  for ticker in dataset_dict:
-    current_price_dict[ticker] = dataset_dict[ticker].close.iloc[i]
-  
-  account.account_value = account.accountValue(current_price_dict)
-
-  for ticker in dataset_dict:
-    # shorthand
-    df = dataset_dict[ticker].df
-    close = dataset_dict[ticker].close
-
-    # calculate necessary technical analysis indicators for each stock/ticker
-    if i < len(df['rsi']) - 1:
-      rsi = round(df['rsi'].iloc[i], 2)
-      rsi_next = round(df['rsi'].iloc[i+1], 2)
-      rsi_prev = round(df['rsi'].iloc[i-1], 2)
-      sma1 = round(df['sma14'].iloc[i], 2)
-      sma2 = round(df['sma30'].iloc[i], 2)
-      sma3 = round(df['sma50'].iloc[i], 2)
-      sma4 = round(df['sma200'].iloc[i], 2)
-      lowerkelt = round(df['lowerkelt'].iloc[i], 2)
-      upperkelt = round(df['upperkelt'].iloc[i], 2)
-      lowerboll = round(df['lowerboll'].iloc[i], 2)
-      upperboll = round(df['upperboll'].iloc[i], 2)
-
-      current_stock_price = close.iloc[i]
-      prev_stock_price = close.iloc[i-1]
-      time = df['Local time'].iloc[i]
-
-
-      if ((rsi < 50) & (sma1>sma2)) or ((rsi > 50) & (sma3>sma4)):
-        account.buy(ticker, current_stock_price, amount_to_buy=1, time=time)
-
-      elif (((rsi > 75) & (sma1<sma2)) or ((rsi < 75) & (sma3<sma4))) & (len(account.portfolio[ticker]) > 0):
-        account.sell(ticker, current_stock_price, amount_to_sell=1, time=time)
